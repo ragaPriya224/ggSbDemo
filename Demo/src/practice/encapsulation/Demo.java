c897d56a6a1b18dcec5496e89ab7359f307648e5
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2023-06-19 09:12:24 +0530

    Add files via upload

---
+++ Demo/src/practice/encapsulation/Demo.java
@@ -0,0 +1,9 @@
+package practice.encapsulation;
+
+public class Demo {
+public static void main(String[] args) {
+	EncapsulationDemo d = new EncapsulationDemo();
+	d.setName("java");
+	System.out.println(d.getName());
+}
+}
