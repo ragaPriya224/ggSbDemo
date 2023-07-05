8e6f25f61fa58630c756c8468263128771677c42
priya <priya@zawqwe.com>
2023-07-05 08:27:25 +0530

    hardcoded  movie with ratings and specific details

--- movie-catalog-service/movie-catalog-service/src/main/java/com/ggDemo/moviecatalogservice/MovieCatalogController.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/ggDemo/moviecatalogservice/MovieCatalogController.java
@@ -1,7 +1,9 @@
 package com.ggDemo.moviecatalogservice;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,13 @@ public class MovieCatalogController {
 	
 	@RequestMapping("/catalog/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId){
-		return Collections.singletonList(new CatalogItem("hi","Test",4));
+		
+		List<Rating> ratingsList = Arrays.asList(new Rating("123",4),
+				new Rating("45",3));
+		return ratingsList.stream().map(rating ->{
+			return new CatalogItem("avengers","descr",4);
+		}).collect(Collectors.toList());
+//		return Collections.singletonList(new CatalogItem("hi","Test",4));
 		
 	}
 }
