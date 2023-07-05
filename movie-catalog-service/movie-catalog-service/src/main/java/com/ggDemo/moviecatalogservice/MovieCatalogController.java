8e904a3403b83e56b992254a3ac9664118468187
priya <priya@zawqwe.com>
2023-07-05 09:03:19 +0530

    use webclient

--- movie-catalog-service/movie-catalog-service/src/main/java/com/ggDemo/moviecatalogservice/MovieCatalogController.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/com/ggDemo/moviecatalogservice/MovieCatalogController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class MovieCatalogController {
+	
+//	@Autowired
+//	private   RestTemplate rt;
+	
 	@Autowired
-	private   RestTemplate rt;
+	private WebClient.Builder webClientBuilder;
 	
 	@RequestMapping("/catalog/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId){
@@ -23,7 +28,15 @@ public class MovieCatalogController {
 		
 //		RestTemplate rt = new RestTemplate();
 		return ratingsList.stream().map(rating ->{
-			Movie movie= rt.getForObject("http://localhost:8082/movies/"+rating.getMoveiId(), Movie.class);
+//			Movie movie= rt.getForObject("http://localhost:8082/movies/"+rating.getMoveiId(), Movie.class);
+			
+			Movie  movie = webClientBuilder.build()
+					.get() //chaining mechanism.get
+					.uri("http://localhost:8082/movies/"+rating.getMoveiId())
+					.retrieve() //fetch
+					.bodyToMono(Movie.class)
+					.block();
+					
 			return new CatalogItem(movie.getName(),"",rating.getRating());
 		}).collect(Collectors.toList());
 //		return Collections.singletonList(new CatalogItem("hi","Test",4));
